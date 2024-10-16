5bd6b532775c8618defdc7eb0743b90fb8eecf31
CassiaValverde <126363351+CassiaValverde@users.noreply.github.com>
2024-10-16 20:36:16 -0300

    Add files via upload

---
+++ exercicios5c.py
@@ -0,0 +1,8 @@
+#verificação positivo negativo
+numero = float(input('Informe o número '))
+if numero>0:
+ print('Ele é positivo')
+elif numero<0:
+ print('Ele é negativo')
+else:
+ print('Ele é zero')
